6dd982d8ee6e0aa67298e315fb626991a5cc9339
Selvaganesan <selva.ganesan@hp.com>
2014-10-03 18:05:29 -0700

    Native Hbase access improvments
    Native hbase access via Trafodion SQL engine now utilizes the enhancements
    made for Trafodion table like pushing mutliple rows to JNI and pre-fetch.
    Also removed the dependency on the ResultIterator for the native hbase access.
    This reduces yet another object created in HTableClient and improves java
    memory usage on the client side.
    
    Cleaned up state changes in the HBase access operators and removed
    redundant code in the JNI layer.
    
    Change-Id: I70ab52917aac64b68b3816b8ad834842a4d8745e

--- sql/executor/HTableClient.java
+++ sql/executor/HTableClient.java
@@ -82,8 +82,6 @@ public class HTableClient {
 	private String tableName;
 
 	private ResultScanner scanner = null;
-	private ResultIterator resultIterator = null;
-	KeyValue lastFetchedCell = null;
 	Result[] getResultSet = null;
 	String lastError;
         RMInterface table = null;
@@ -308,7 +306,7 @@ public class HTableClient {
 
 	public boolean startGet(long transID, byte[] rowID, 
                      Object[] columns,
-		     long timestamp, boolean directRow) throws IOException {
+		     long timestamp) throws IOException {
 
 	    if (logger.isTraceEnabled()) logger.trace("Enter startGet(" + tableName + 
 			     " #cols: " + ((columns == null) ? 0:columns.length ) +
@@ -366,8 +364,7 @@ public class HTableClient {
 	}
 
 	public boolean startGet(long transID, Object[] rows,
-			Object[] columns, long timestamp,
-			boolean directRow) 
+			Object[] columns, long timestamp)
                         throws IOException {
 
 		if (logger.isTraceEnabled()) logger.trace("Enter startGet(multi-row) " + tableName);
@@ -395,33 +392,7 @@ public class HTableClient {
 		} else {
 			getResultSet = table.get(listOfGets);
 		}
-		if (directRow) 
-			return true;
-		else {
-			if (getResultSet.length > 0) {
-			        resultIterator = new ResultIterator(getResultSet);
-			} else
-				resultIterator = null;
-		}
 		return true;
-	}
-
-	public boolean scanFetch() throws IOException {
-		if (logger.isTraceEnabled()) logger.trace("Enter scanFetch() " + tableName);
-		if (resultIterator == null) {
-			return false;
-		}
-
-		lastFetchedCell = resultIterator.nextCell();
-		if (lastFetchedCell == null) {
-			return false; 
-		}
-		return true;
-	}
-
-	public boolean getFetch() throws IOException {
-		if (logger.isTraceEnabled()) logger.trace("Enter getFetch() " + tableName);
-		return scanFetch();
 	}
 
 	public int fetchRows() throws IOException, 
@@ -471,12 +442,19 @@ public class HTableClient {
 		if (result == null || result.length == 0)
 			return 0; 
 		int rowsReturned = result.length;
+		int numTotalCells = 0;
+		if (numColsInScan == 0)
+		{
+			for (int i = 0; i < result.length; i++) {	
+				numTotalCells += result[i].size();
+			}
+		}
+		else
 		// There can be maximum of 2 versions per kv
 		// So, allocate place holder to keep cell info
 		// for that many KVs
-		int numTotalCells = 2 * rowsReturned * numColsInScan;
+			numTotalCells = 2 * rowsReturned * numColsInScan;
 		int numColsReturned;
-		HashMap<String, Integer>  kvMap = null;
 		Cell[] kvList;
 		Cell kv;
 
@@ -538,13 +516,6 @@ public class HTableClient {
 		return rowsReturned;	
 	}		
 	
-	public KeyValue getLastFetchedCell() {
-		if (logger.isTraceEnabled()) logger.trace("Enter getLastFetchedCell() ");
-		if (lastFetchedCell == null)
-			if (logger.isTraceEnabled()) logger.trace("  Returning empty.");
-		return lastFetchedCell;
-	}
-
 	public boolean deleteRow(long transID, byte[] rowID, 
 				 Object[] columns,
 				 long timestamp) throws IOException {
@@ -844,8 +815,6 @@ public class HTableClient {
 			executorService.shutdown();
 			executorService = null;
 		}
-		resultIterator = null;
-		lastFetchedCell = null;
 		getResultSet = null;
 		if (cleanJniObject) {
 			if (jniObject != 0)
