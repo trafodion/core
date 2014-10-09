950f2ee06a44e3a3f8bee05fb0a7a6253bfc4bd7
Oliver Bucaojit <oliver.bucaojit@hp.com>
2014-10-09 01:57:37 +0000

    TransactionalScanner nextCallSeq fix
    Possible fix for OutOfOrderScannerNextException issue being hit.
    The nextCallSeq value was not being set correctly in the
    client-side scanner.  May be the reason for the above exception.
    
    Change-Id: Ic7a20116dd915cfc204a90365c4feb2c564d8d26

--- sqf/src/seatrans/hbase-trx/src/main/java/org/apache/hadoop/hbase/client/transactional/TransactionalScanner.java
+++ sqf/src/seatrans/hbase-trx/src/main/java/org/apache/hadoop/hbase/client/transactional/TransactionalScanner.java
@@ -28,10 +28,7 @@ import com.google.protobuf.ByteString;
 
 
 /*
- *   Simple Transaction Scanner, doesn't have full functionality ie caching
- *                               also does not avoid splitting, that will require an extra call
- *                               such as openScanner to be run on all regions and the region names 
- *                               along with the scanner ID's saved 
+ *   Simple Transaction Scanner
  */
 public class TransactionalScanner extends AbstractClientScanner {
 	private final Log LOG = LogFactory.getLog(this.getClass());
@@ -40,11 +37,11 @@ public class TransactionalScanner extends AbstractClientScanner {
 	public TransactionState ts;
 	public TransactionalTable ttable;
 	protected boolean closed = false;
-        protected boolean doNotCloseOnLast = true;
-        protected int nbRows = 100;
+    protected boolean doNotCloseOnLast = true;
+    protected int nbRows = 100;
+    protected long nextCallSeq = 0;
 	private boolean firstEntry = true;
 	public HRegionInfo currentRegion;
-	//public String currentRegionName;
 	public byte[] currentBeginKey;
 	public byte[] currentEndKey;
 	protected final LinkedList<Result> cache = new LinkedList<Result>();
@@ -56,9 +53,9 @@ public class TransactionalScanner extends AbstractClientScanner {
 		this.scannerID = scannerID;
 		this.ts = ts;
 		this.ttable = ttable;
-                this.nbRows = scan.getCaching();
-                if (nbRows <= 0)
-                  nbRows = 100;
+        this.nbRows = scan.getCaching();
+        if (nbRows <= 0)
+            nbRows = 100;
 		try {
 			nextScanner(false);
 		}catch (IOException e) {
@@ -66,29 +63,23 @@ public class TransactionalScanner extends AbstractClientScanner {
 		}
 	}
 
-    // returns true if the passed region endKey
     protected boolean checkScanStopRow(final byte [] endKey) {
       if (this.scan.getStopRow().length > 0) {
-        // there is a stop row, check to see if we are past it.
         byte [] stopRow = scan.getStopRow();
         int cmp = Bytes.compareTo(stopRow, 0, stopRow.length,
           endKey, 0, endKey.length);
         if (cmp <= 0) {
-          // stopRow <= endKey (endKey is equals to or larger than stopRow)
-          // This is a stop.
           return true;
         }
       }
-      return false; //unlikely.
+      return false;
     }
     
 	@Override
 	public void close() {
-		// Make call to closeScanner(), close all regions at once for now
-		// TODO: Still needs work.  Should close a particular scanner in nextScanner()
-		LOG.trace("close() -- ENTRY txID: " + ts.getTransactionId());
+		if(LOG.isTraceEnabled()) LOG.trace("close() -- ENTRY txID: " + ts.getTransactionId());
 		if(closed == true) {
-			LOG.trace("close()  already closed -- EXIT txID: " + ts.getTransactionId());
+			if(LOG.isTraceEnabled()) LOG.trace("close()  already closed -- EXIT txID: " + ts.getTransactionId());
 			return;
 		}
 		 Batch.Call<TrxRegionService, CloseScannerResponse> callable =
@@ -101,9 +92,7 @@ public class TransactionalScanner extends AbstractClientScanner {
 			      public CloseScannerResponse call(TrxRegionService instance) throws IOException {
 			        org.apache.hadoop.hbase.coprocessor.transactional.generated.TrxRegionProtos.CloseScannerRequest.Builder builder = CloseScannerRequest.newBuilder();
 			        builder.setTransactionId(ts.getTransactionId());
-			        builder.setScannerId(scannerID);
-			        
-			        // TODO: put region name at some point
+			        builder.setScannerId(scannerID);			        
 			        builder.setRegionName(ByteString.copyFromUtf8(""));
 
 			        instance.closeScanner(controller, builder.build(), rpcCallback);
@@ -127,20 +116,17 @@ public class TransactionalScanner extends AbstractClientScanner {
 			        boolean hasException = cresponse.getHasException();
 			        String exception = cresponse.getException();
 			        if (hasException)
-			          LOG.trace("  CloseScannerResponse exception " + exception );
+			        	if(LOG.isTraceEnabled()) LOG.trace("  CloseScannerResponse exception " + exception );
 			      }
 	    this.closed = true;
-	    LOG.trace("close() -- EXIT txID: " + ts.getTransactionId());
+	    if(LOG.isTraceEnabled()) LOG.trace("close() -- EXIT txID: " + ts.getTransactionId());
 		
 	}
 	
 	protected boolean nextScanner(final boolean done) throws IOException{
-		LOG.trace("nextScanner() -- ENTRY txID: " + ts.getTransactionId());
-		
-		//byte [] localStartKey;
-		
+		if(LOG.isTraceEnabled()) LOG.trace("nextScanner() -- ENTRY txID: " + ts.getTransactionId());
 		if(this.currentBeginKey != null) {
-			LOG.trace("nextScanner() currentBeginKey += null txID: " + ts.getTransactionId());
+			if(LOG.isTraceEnabled()) LOG.trace("nextScanner() currentBeginKey += null txID: " + ts.getTransactionId());
                         if (doNotCloseOnLast)
 			  close();
 			byte [] endKey = this.currentRegion.getEndKey();
@@ -150,27 +136,23 @@ public class TransactionalScanner extends AbstractClientScanner {
 			   checkScanStopRow(endKey) ||
 			   done) {
 				
-				LOG.trace("nextScanner() -- EXIT -- returning false txID: " + ts.getTransactionId());
+				if(LOG.isTraceEnabled()) LOG.trace("nextScanner() -- EXIT -- returning false txID: " + ts.getTransactionId());
 				return false;				
 			}
-			//localStartKey = endKey;
 			this.currentBeginKey = TransactionManager.binaryIncrementPos(endKey,1);
 			
 		}
 		else {
-			// currentBeginKey is NULL so this is first entry
-			//localStartKey = this.scan.getStartRow();
 			this.currentBeginKey = this.scan.getStartRow();					
 		}
 		
 		this.currentRegion = ttable.getRegionLocation(this.currentBeginKey).getRegionInfo();
-		LOG.trace("Region Info: " + currentRegion.getRegionNameAsString());
-		this.currentEndKey = TransactionManager.binaryIncrementPos(currentRegion.getEndKey(), -1);
-		//endKey = TransactionManager.binaryIncrementPos(endKey_orig, -1);
-		// This is where we open and get the scanner with the startKey, endKey
+		if(LOG.isTraceEnabled()) LOG.trace("Region Info: " + currentRegion.getRegionNameAsString());
+		if(this.currentEndKey != HConstants.EMPTY_END_ROW)
+			this.currentEndKey = TransactionManager.binaryIncrementPos(currentRegion.getEndKey(), -1);
 		
 		this.closed = false;
-	   Batch.Call<TrxRegionService, OpenScannerResponse> callable =
+	        Batch.Call<TrxRegionService, OpenScannerResponse> callable =
 		        new Batch.Call<TrxRegionService, OpenScannerResponse>() {
 		      ServerRpcController controller = new ServerRpcController();
 		      BlockingRpcCallback<OpenScannerResponse> rpcCallback =
@@ -181,10 +163,6 @@ public class TransactionalScanner extends AbstractClientScanner {
 		        org.apache.hadoop.hbase.coprocessor.transactional.generated.TrxRegionProtos.OpenScannerRequest.Builder builder = OpenScannerRequest.newBuilder();
 		        builder.setTransactionId(ts.getTransactionId());
 		        builder.setRegionName(ByteString.copyFromUtf8(currentRegion.getRegionNameAsString()));
-
-		        //Scan scan = new Scan();
-		        //scan.addColumn(FAMILY, Bytes.toBytes(1));
-
 		        builder.setScan(ProtobufUtil.toScan(scan));
 
 		        instance.openScanner(controller, builder.build(), rpcCallback);
@@ -198,7 +176,6 @@ public class TransactionalScanner extends AbstractClientScanner {
 		      result = ttable.coprocessorService(TrxRegionService.class, 
 		    		  							 this.currentBeginKey,
 		    		  							 this.currentEndKey,
-		    		  							 //this.currentEndKey, 
 		    		  							 callable);
 		      		      
 		    } catch (Throwable e) {
@@ -211,31 +188,28 @@ public class TransactionalScanner extends AbstractClientScanner {
 		        String exception = oresponse.getException();
 		        boolean hasException = oresponse.getHasException();
 		        if (hasException) {
-		          //LOG.error("  OpenScannerResponse exception " + exception );
-		          // Exception is ok for now
-		          LOG.trace("nextScanner() -- EXIT -- encountered EXCEPTION returning false txID: " + ts.getTransactionId());		          		          
+		        	if(LOG.isTraceEnabled()) LOG.trace("nextScanner() -- EXIT -- encountered EXCEPTION returning false txID: " + ts.getTransactionId() + 
+		        		  exception);		          		          
 		        }
 		        else{
 		          
 		        	this.scannerID = oresponse.getScannerId();
-		        	LOG.trace("  OpenScannerResponse scannerId is " + this.scannerID );
+		        	if(LOG.isTraceEnabled()) LOG.trace("  OpenScannerResponse scannerId is " + this.scannerID );
 		        }
 		      }
 
-		LOG.trace("nextScanner() -- EXIT -- returning true txID: " + ts.getTransactionId());
+		this.nextCallSeq = 0;
+		if(LOG.isTraceEnabled()) LOG.trace("nextScanner() -- EXIT -- returning true txID: " + ts.getTransactionId());
 		return true;
 	}
 
 	@Override
 	public Result next() throws IOException {
-		// Commenting out ENTRY/EXIT lines since they fill up logs, uncomment for
-		//    debugging purposes
-		//LOG.trace("next() -- ENTRY txID: " + ts.getTransactionId());
-		
 		if(cache.size() == 0 && firstEntry == true) { 
 			firstEntry = false;
 			do { 
-				LOG.trace("next() before coprocessor PerformScan call txID: " + ts.getTransactionId());		
+				if(LOG.isTraceEnabled()) LOG.trace("next() before coprocessor PerformScan call txID: " + ts.getTransactionId());	
+				final long nextCallSeqInput = this.nextCallSeq; 
 				Batch.Call<TrxRegionService, PerformScanResponse> callable = 
 				        new Batch.Call<TrxRegionService, PerformScanResponse>() {
 				      ServerRpcController controller = new ServerRpcController();
@@ -253,7 +227,7 @@ public class TransactionalScanner extends AbstractClientScanner {
 				          builder.setCloseScanner(false);
                                         else
 				          builder.setCloseScanner(true);
-				        builder.setNextCallSeq(0);
+				        builder.setNextCallSeq(nextCallSeqInput);
 
 				        instance.performScan(controller, builder.build(), rpcCallback);
 				        return rpcCallback.get();        
@@ -269,7 +243,6 @@ public class TransactionalScanner extends AbstractClientScanner {
 				      presult = ttable.coprocessorService(TrxRegionService.class, currentBeginKey, currentEndKey, callable);
 				    } catch (Throwable e) {
 				    	LOG.error("ERROR when calling PerformScan coprocessor" + e.toString());
-				    	//e.printStackTrace();     
 				    }
 
 				      int count = 0;
@@ -283,10 +256,12 @@ public class TransactionalScanner extends AbstractClientScanner {
 				        if (presponse.getHasException())
 				        {
 				          String exception = presponse.getException();
-				          LOG.trace("  PerformScanResponse exception " + exception );
+				          if(LOG.isTraceEnabled()) LOG.trace("  PerformScanResponse exception " + exception );
 				        }
 				        else
-				        {
+				        {				        	
+				          this.nextCallSeq = presponse.getNextCallSeq();
+				          if(LOG.isTraceEnabled()) LOG.trace("next() nextCallSeq: " + this.nextCallSeq);	
 				          count = presponse.getResultCount();
 				          results = 
 				            new org.apache.hadoop.hbase.protobuf.generated.ClientProtos.Result[count];
@@ -300,19 +275,19 @@ public class TransactionalScanner extends AbstractClientScanner {
 				            hasMore = presponse.getHasMore();
 				            results[i] = result;
 				            result = null;
-				            LOG.trace("  PerformScan response count " + count + ", hasMore is " + hasMore + ", result " + results[i] );
+				            if(LOG.isTraceEnabled()) LOG.trace("  PerformScan response count " + count + ", hasMore is " + hasMore + ", result " + results[i] );
 				          }
 				        }
 				      }
 				      
 				      if(!hasMore) {
-				    	  LOG.trace("hasMore is false");
+				    	  if(LOG.isTraceEnabled()) LOG.trace("hasMore is false");
 				    	  if(nextScanner(false) == true){
 				    		  LOG.trace("nextScanner == true, continuing");
 				    		  continue;
 				    	  }
 				    	  else {
-                              LOG.trace("nextScanner == false, break");
+				    		  if(LOG.isTraceEnabled()) LOG.trace("nextScanner == false, break");
 				    		  break;
 				    	  }
 				    		  
@@ -322,13 +297,8 @@ public class TransactionalScanner extends AbstractClientScanner {
 		}
 		
 		if (cache.size() > 0)  {
-			//LOG.trace("next() -- EXIT txID: " + ts.getTransactionId());
 			return cache.poll();
 		}
-
-		
-		// Exhausted scanner before calling close, then return null
-		//LOG.trace("next() -- EXIT -- returning null txID: " + ts.getTransactionId());
 		return null;
 	}
 
