32fe9959f71341b11e2d6e261e3c53eb1e06f376
john-deroo <john.deroo@hp.com>
2015-02-03 01:03:53 +0000

    bp/dtm-local-transaction
    Change-Id: Ib128a2c84ba4a1b60c416f4d3f570f1bda2d75eb

---
+++ sqf/src/tm/tmseqnum.h
@@ -0,0 +1,73 @@
+// @@@ START COPYRIGHT @@@
+//
+// (C) Copyright 2013-2014 Hewlett-Packard Development Company, L.P.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+// @@@ END COPYRIGHT @@@
+
+#ifndef TMSEQNUM_H_
+#define TMSEQNUM_H_
+
+#include <stdlib.h>
+#include "dtm/tm.h"
+#include "dtm/tmtransid.h"
+#include "dtm/tmtransaction.h"
+#include "tmmap.h"
+
+// --------------------------------------------------------------------------
+// CtmSeqNum
+// -- Provides TM Library with sequence number blocks 
+// --------------------------------------------------------------------------
+class CtmSeqNum
+{
+private:
+   #define MAX_SEQNUM (UINT_MAX-1)
+   bool iv_firstTime;
+   uint32 iv_nextSeqNum;
+   uint32 iv_seqNumBlock_start;
+   uint32 iv_seqNumBlock_count;
+public: 
+    CtmSeqNum() {
+       iv_firstTime = true;
+       iv_nextSeqNum = -1;
+       iv_seqNumBlock_start = -1;
+       iv_seqNumBlock_count = -1;
+    }
+
+    ~CtmSeqNum();
+
+    uint32 nextSeqNum() {
+       uint32 lv_returnSeqNum = 0;
+
+       //  Get next sequence number block or rollover if at the end of the block or MAX_SEQNUM
+       if (iv_firstTime ||
+           (iv_nextSeqNum >= iv_seqNumBlock_start + iv_seqNumBlock_count) ||
+           (iv_seqNumBlock_start >= MAX_SEQNUM) || (iv_nextSeqNum >= MAX_SEQNUM)) {
+          iv_firstTime = false;
+          // End of sequence number block or maximum sequence number - get new block
+          short lv_err = DTM_GETNEXTSEQNUMBLOCK(iv_seqNumBlock_start, iv_seqNumBlock_count);
+          if (lv_err) {
+             printf("Sequence number block fetch failed with error %d.\n", lv_err);
+             abort();
+          }
+          else
+             iv_nextSeqNum = iv_seqNumBlock_start;
+       }
+       lv_returnSeqNum = iv_nextSeqNum++;
+       return lv_returnSeqNum;
+    }
+};
+
+
+#endif
